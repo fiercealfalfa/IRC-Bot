9668ce2a0b8052b6d230481f5375525baaa82724
fiercealfalfa <allewellyn@gmail.com>
2015-04-23 15:14:58 -0500

    IRC Bot works
    Not 100% it is ponging right, but the bot joins a server, auths its name
    and joins a channel.

--- ircbot.py
+++ ircbot.py
@@ -1,8 +1,10 @@
 #IRC bot
-#Written with hrlp from wiki.shellium.org and the wayback machine (because wiki.shellium.org appears to be down)
+#Written with help from wiki.shellium.org and the wayback machine (because wiki.shellium.org appears to be down)
 
 #import libraries
 import socket
+import ssl
+import time
 
 #Server, channel, nick, and password variables
 from ircpasswords import settings
@@ -10,33 +12,47 @@ from ircpasswords import settings
 server = settings['server']
 channel = settings['channel']
 botnick = settings['botnick']
+password = settings['botpass']
 
 #Respond to server pings
 def ping():
-	ircsock.send("PONG :Pong\n")
+	irc.send("PONG " + ircmsg.split()[1] + "\r\n")
+	print("PONG " + ircmsg.split()[1] + "\r\n") 
 	
 	
 #Send Messages
 def sendmsg(chan , msg):
-	ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
+	irc.send("PRIVMSG "+ chan +" :"+ msg +"\n")
 
 #Join Channels
 def joinchan(chan):
-	ircsock.send("JOIN "+ chan +"\n")
+	irc.send("JOIN "+ chan +"\n")
 
-#Simple Reply to Hello
-def hello(newnick):
-	ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+#Simple Reply to Hello 
+def hello():
+	irc.send("PRIVMSG "+ channel +" :Hello!\n")
 
 #Connecting to server, joining channel and authenticating
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667))
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :devCodeIRCbot - A work in Progress")
-ircsock.send("NICK "+botnick +"\n")
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Connecting to : "+ server
+#irc = ssl.wrap_socket(irc_C)
+irc.connect((server,6667))
+time.sleep(5)
+#irc.setblocking(False)
+#irc.send("PASS %s\n" % (password))
+irc.send("NICK "+ botnick +"\n")
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :devCodeIRCbot - A work in Progress")
+
+time.sleep(5)
+irc.send("PRIVMSG NickServ identify %s %s\r\n" % (botnick, password))
+time.sleep(5)
+irc.send("JOIN " + channel +"\n")
+
 
 
 while 1:
-	ircmsg = ircsock.recv(2048) #receiving info from IRC
+	ircmsg = irc.recv(2048) #receiving info from IRC
 	ircmsg = ircmsg.strip('\n\r')
 	print(ircmsg)
 	
